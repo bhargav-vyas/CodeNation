75de358cfbebdb34445836676190ef440f66ca49
Bhargav <vyasb1503@gmail.com>
2025-05-05 23:15:44 +0530

    this is the starting of the project in that  first the dependency are added

---
+++ src/main/java/com/tka/CodeNationApplication.java
@@ -0,0 +1,13 @@
+package com.tka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeNationApplication.class, args);
+	}
+
+}
