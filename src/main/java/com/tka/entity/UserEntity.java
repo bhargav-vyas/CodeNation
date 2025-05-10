17c2a0f3fea9368b490b1f0f5e7d431b67962d47
Bhargav <vyasb1503@gmail.com>
2025-05-10 22:25:10 +0530

    this is the  commit of the entity

--- src/main/java/com/tka/entity/UserEntity.java
+++ src/main/java/com/tka/entity/UserEntity.java
@@ -1,9 +1,13 @@
 package com.tka.entity;
 
+import org.hibernate.mapping.UniqueKey;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
@@ -12,6 +16,7 @@ public class UserEntity {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private  Long id;
 	private  String username;
+	@Column(unique = true)
 	private String email;
 	private String  password;
 
