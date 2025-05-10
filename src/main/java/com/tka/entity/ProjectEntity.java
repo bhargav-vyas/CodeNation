19f4a66c9505a69412def2394d18a4426ba37970
Bhargav <vyasb1503@gmail.com>
2025-05-10 22:35:58 +0530

    this is the  project entity

---
+++ src/main/java/com/tka/entity/ProjectEntity.java
@@ -0,0 +1,21 @@
+package com.tka.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class ProjectEntity {
+ 
+	private Long id;
+	private  String title;
+	private String name;
+	private String description;
+	private String  language;
+	private String repoitoryUrl;
+	@ManyToOne
+	private Set<Technology> technologies;
+}
