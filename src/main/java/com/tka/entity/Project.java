cdf79b7cfe3bdedebd3afa2d8f382d3d3ddf2eab
Bhargav <vyasb1503@gmail.com>
2025-05-12 22:13:48 +0530

    changes in the  entity and the adition of the annotation

--- src/main/java/com/tka/entity/Project.java
+++ src/main/java/com/tka/entity/Project.java
@@ -3,13 +3,16 @@ package com.tka.entity;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity
 @Data
 public class Project {
- 
+ @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private  String title;
 	private String name;
