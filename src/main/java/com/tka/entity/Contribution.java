1514e1c1fd2732daa0d77f3e7c578cfc3d9e1354
Bhargav <vyasb1503@gmail.com>
2025-05-12 22:12:15 +0530

    changes in the  entity and the adition of the annotation

--- src/main/java/com/tka/entity/Contribution.java
+++ src/main/java/com/tka/entity/Contribution.java
@@ -3,13 +3,16 @@ package com.tka.entity;
 import org.apache.catalina.User;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
-
+import jakarta.persistence.Id;
 @Entity
 @Data
-public class Contribution {
 
+public class Contribution {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long Id;
 	@ManyToOne
 	 private User Contrubution;
--- src/main/java/com/tka/entity/Technology.java
+++ src/main/java/com/tka/entity/Technology.java
@@ -1,5 +1,8 @@
 package com.tka.entity;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Technology {
 
 }
