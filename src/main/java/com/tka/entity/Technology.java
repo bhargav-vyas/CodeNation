449edb55293c56cbd5ddfef74f84f0c98ec62ade
Bhargav <vyasb1503@gmail.com>
2025-05-12 22:15:37 +0530

    changes in the  entity and the adition of the annotation

--- src/main/java/com/tka/entity/Technology.java
+++ src/main/java/com/tka/entity/Technology.java
@@ -1,8 +1,11 @@
 package com.tka.entity;
 
 import jakarta.persistence.Entity;
-
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 @Entity
 public class Technology {
-
+@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private  Long Id;
 }
