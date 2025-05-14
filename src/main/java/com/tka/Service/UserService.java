2b4c5f237d585b7e39cccbdb3dec4d0838c859ca
Bhargav <vyasb1503@gmail.com>
2025-05-14 23:56:06 +0530

    ading the anotation of the  autowire to the service

--- src/main/java/com/tka/Service/UserService.java
+++ src/main/java/com/tka/Service/UserService.java
@@ -1,9 +1,13 @@
 package com.tka.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.tka.Reporisetory.UserRepository;
 
 @Service
 public class UserService {
 
-	
+	@Autowired
+	private UserRepository userRepository;	
 }
