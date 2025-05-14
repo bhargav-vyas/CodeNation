8a587e97be29374677cf6b2baa19f48fa7b03606
Bhargav <vyasb1503@gmail.com>
2025-05-14 23:54:55 +0530

    ading the Repository package

--- src/main/java/com/tka/Reporisetory/UserRepository.java
+++ src/main/java/com/tka/Reporisetory/UserRepository.java
@@ -1,5 +1,10 @@
 package com.tka.Reporisetory;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tka.Entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
 
 }
