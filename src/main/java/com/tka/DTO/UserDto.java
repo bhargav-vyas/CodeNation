ee4ffaee303a23d9591dc345af7148fbedcd64fc
Bhargav <vyasb1503@gmail.com>
2025-05-15 22:55:39 +0530

    this is the  api of the resistration in the user entity

--- src/main/java/com/tka/DTO/UserDto.java
+++ src/main/java/com/tka/DTO/UserDto.java
@@ -16,21 +16,21 @@ public class UserDto {
 	public String getUsername() {
 		return username;
 	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	
+//	public void setUsername(String username) {
+//		this.username = username;
+//	}
+//	public String getEmail() {
+//		return email;
+//	}
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//	public String getPassword() {
+//		return password;
+//	}
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+//
+//	
 }
