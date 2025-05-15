b1a4ea4859c473888216c5a1e3b0cab573eeba9b
Bhargav <vyasb1503@gmail.com>
2025-05-15 22:48:19 +0530

    this is the  api of the resistration in the user entity

--- src/main/java/com/tka/Controller/UserController.java
+++ src/main/java/com/tka/Controller/UserController.java
@@ -1,9 +1,13 @@
 package com.tka.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tka.Entity.User;
 import com.tka.Service.UserService;
 
 @RestController
@@ -11,6 +15,12 @@ import com.tka.Service.UserService;
 public class UserController {
 	@Autowired
 	private UserService userService;
+	
+	@PostMapping
+	 public ResponseEntity<String>  resistration(@RequestBody User user){
+		userService.resistration(user);
+		return ResponseEntity.ok("user resistration sucessful;");
+	}
 	
 
 }
--- src/main/java/com/tka/Service/UserService.java
+++ src/main/java/com/tka/Service/UserService.java
@@ -3,11 +3,17 @@ package com.tka.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tka.Entity.User;
 import com.tka.Reporisetory.UserRepository;
 
 @Service
 public class UserService {
 
 	@Autowired
-	private UserRepository userRepository;	
+	private UserRepository userRepository;
+
+	public void resistration(User user) {
+		userRepository.save(user);
+		
+	}	
 }
