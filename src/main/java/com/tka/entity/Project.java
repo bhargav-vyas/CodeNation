d2c03a8c5c37bc800c9cce449c0e4cefb3f82aae
Bhargav <vyasb1503@gmail.com>
2025-05-16 21:06:07 +0530

    change in  the  entity

--- src/main/java/com/tka/entity/Project.java
+++ src/main/java/com/tka/entity/Project.java
@@ -1,25 +1,25 @@
-//package com.tka.Entity;
-//
-//import java.util.Set;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import lombok.Data;
-//
-//
-//@Data
-//@Entity
-//public class Project {
-// @Id
-// @GeneratedValue(strategy = GenerationType.IDENTITY)
-//	private Long id;
-//	private  String title;
-//	private String name;
-//	private String description;
-//	private String  language;
-//	private String repoitoryUrl;
-////	@ManyToOne
-////	private Set<Technology> technologies;
-//}
+package com.tka.Entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+@Data
+@Entity
+public class Project {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private  String title;
+	private String name;
+	private String description;
+	private String  language;
+	private String repoitoryUrl;
+//	@ManyToOne
+//	private Set<Technology> technologies;
+}
