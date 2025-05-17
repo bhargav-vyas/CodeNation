c9caf4a20886fbe08c9f28f132d99423a56c7b64
Bhargav <vyasb1503@gmail.com>
2025-05-17 21:10:28 +0530

    changes in the   User dto

--- src/main/java/com/tka/DTO/UserDto.java
+++ src/main/java/com/tka/DTO/UserDto.java
@@ -1,12 +1,16 @@
 package com.tka.DTO;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 
 public class UserDto {
 	private  Long id;
 	 @Size(min = 2, max = 50, message = "Name must be between 2-50 characters")
 	private  String username;
+	 @Email(message = "EmailShould be valid")
 	private String email;
+	 @Size(min = 8 , max= 100 ,message = "password must be between 8-100 charactrs")
 	private String  password;
 	public Long getId() {
 		return id;
