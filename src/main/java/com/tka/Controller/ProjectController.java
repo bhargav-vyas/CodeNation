70b9e3516c4406135affe04090a719b76baf33bf
Bhargav <vyasb1503@gmail.com>
2025-06-07 22:45:21 +0530

    this is   adition of the  method add project

--- src/main/java/com/tka/Controller/ProjectController.java
+++ src/main/java/com/tka/Controller/ProjectController.java
@@ -1,5 +1,7 @@
 package com.tka.Controller;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +19,9 @@ public class ProjectController {
 	private ProjectService projectService;
 	@PostMapping("/add")
 	public  String addProject(@RequestBody Project project) {
-		project.setCreatedDate(null)
+        project.setCreatedDate(LocalDate.now());
+        projectService.addProject(project);
+        return "project added sucessful";
 		
 	}
 }
--- src/main/java/com/tka/Service/ProjectService.java
+++ src/main/java/com/tka/Service/ProjectService.java
@@ -2,7 +2,14 @@ package com.tka.Service;
 
 import org.springframework.stereotype.Service;
 
+import com.tka.Entity.Project;
+
 @Service
 public class ProjectService {
+
+	public void addProject(Project project) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
