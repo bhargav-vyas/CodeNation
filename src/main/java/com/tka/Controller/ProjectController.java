e8bab21e0b014865df85e45641c784595aff28f9
Bhargav <vyasb1503@gmail.com>
2025-06-07 22:36:34 +0530

    this is  projectcontroller  class

--- src/main/java/com/tka/Controller/ProjectController.java
+++ src/main/java/com/tka/Controller/ProjectController.java
@@ -1,5 +1,18 @@
 package com.tka.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tka.Service.ProjectService;
+
+@RestController
+@RequestMapping("/project")
 public class ProjectController {
 
+	@Autowired
+	private ProjectService projectService;
+	@PostMapping("/add")
+	public 
 }
