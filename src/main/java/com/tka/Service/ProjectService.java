01a4e974d5643615e7726b368eb527e906955457
Bhargav <vyasb1503@gmail.com>
2025-06-10 22:26:30 +0530

    implement  the   api in the service

--- src/main/java/com/tka/Service/ProjectService.java
+++ src/main/java/com/tka/Service/ProjectService.java
@@ -1,14 +1,18 @@
 package com.tka.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tka.Entity.Project;
+import com.tka.Reporisetory.ProjectReporisetory;
 
 @Service
 public class ProjectService {
+@Autowired 
+  private ProjectReporisetory projectReporisetory;
 
 	public void addProject(Project project) {
-		// TODO Auto-generated method stub
+		projectService.save(project);
 		
 	}
 
