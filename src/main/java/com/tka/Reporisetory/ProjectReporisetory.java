9b2411f82633b0122f5afea1a73d338d00245300
Bhargav <vyasb1503@gmail.com>
2025-06-10 22:14:57 +0530

    this is the   interface of the  jpa Repository

--- src/main/java/com/tka/Reporisetory/ProjectReporisetory.java
+++ src/main/java/com/tka/Reporisetory/ProjectReporisetory.java
@@ -1,5 +1,10 @@
 package com.tka.Reporisetory;
 
-public class ProjectReporisetory {
+import org.springframework.boot.autoconfigure.web.servlet.JerseyApplicationPath;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tka.Entity.Project;
+
+public interface ProjectReporisetory extends JpaRepository<Project, Long> {
 
 }
